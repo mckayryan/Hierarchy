7d64825023fb6780b0db05e0db19fb3e8e55ba6a
Ryan <ryan.mckay@student.unsw.edu.au>
2016-07-06 11:34:36 +1000

    control.py: basic description only

---
+++ control.py
@@ -0,0 +1,5 @@
+####    Hierarchy
+##      Creators: Adam Stiles and Ryan McKay
+##      July 2016
+
+## Heirarchy is a game of survival that urges the player to discover it means to exist as a Human.
