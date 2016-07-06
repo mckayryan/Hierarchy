e9127aeee088a12ed7c9c37a3107738f59e34655
adamstiles <mozric@gmail.com>
2016-07-06 14:26:54 +1000

    Minor Line Insert

--- control.py
+++ control.py
@@ -1,5 +1,6 @@
 ####    Hierarchy
 ##      Creators: Adam Stiles and Ryan McKay
 ##      July 2016
+##		Made in Python
 
 ## Heirarchy is a game of survival that urges the player to discover what it means to be Human.
